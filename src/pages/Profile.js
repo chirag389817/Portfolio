794e34d6840b4ba35883e4596e3495cfa0bca310
Chirag <chirag389817@gmail.com>
2023-01-04 20:36:53 +0530

    changed my image

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,11 +5,11 @@ const pimage = require("../images/pimage.jpg");
 function Profile() {
     return (
         <div className="profile-container">
-            <img src={pimage} alt="pimage" id="pimage" />
             <div className="ptext">
                 <p className="name">Patel Chirag</p>
                 <p className="skill">A developer and freelancer</p>
             </div>
+            <img src={pimage} alt="pimage" id="pimage" />
         </div>
     );
 }
